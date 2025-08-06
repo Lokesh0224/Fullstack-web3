00aa6130a401729be0016c18d2dd0a9379a31062
Lokesh0224 <lokeshk20019@gmail.com>
2025-08-06 22:28:31 +0530

    nextjs added rainbowkit wagmi tanstackQuery

---
+++ ts-tsender-ui-cu/src/app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "TSender",
+  description: "A simple and fast email sender",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
