ba943a13aeb87063f9b44cfc349d1661a9d97740
Lokesh0224 <lokeshk20019@gmail.com>
2025-08-07 18:36:25 +0530

    walletconnect id in config

--- ts-tsender-ui-cu/src/rainbowKitConfig.tsx
+++ ts-tsender-ui-cu/src/rainbowKitConfig.tsx
@@ -1,1 +1,11 @@
 "use client";
+import { getDefaultConfig } from '@rainbow-me/rainbowkit'
+import {anvil, zksync} from "wagmi/chains"
+
+const config= getDefaultConfig({
+    appName: "TSender", 
+    projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!, 
+    chains: [anvil, zksync], 
+    ssr: false
+
+})
