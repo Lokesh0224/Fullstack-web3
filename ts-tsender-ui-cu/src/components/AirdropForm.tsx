2816c2aabdd472a453bae071186770478f1357ff
Lokesh0224 <lokeshk20019@gmail.com>
2025-08-08 13:46:34 +0530

    added the token address input field

--- ts-tsender-ui-cu/src/components/AirdropForm.tsx
+++ ts-tsender-ui-cu/src/components/AirdropForm.tsx
@@ -1,3 +1,20 @@
+"use client"
+
+import { useState } from "react";
+import InputForm from "./ui/InputField";
+
 export default function AirdropForm(){
 
+    const [tokenAddress, setTokenAddress] = useState("")
+    return(
+        <div>
+            <InputForm 
+                label="Token Address"
+                placeholder="0x"
+                value={tokenAddress}
+                onChange={e => setTokenAddress(e.target.value)}
+            />
+            Hi from AirDrop
+        </div>
+    )
 }
--- ts-tsender-ui-cu/src/components/header.tsx
+++ ts-tsender-ui-cu/src/components/header.tsx
@@ -3,17 +3,19 @@ import {FaGithub} from 'react-icons/fa'
 
 export default function Header(){
     return(
-        <header className="flex items-center justify-between p-4 bg-red-500 ">
+        <header className="flex items-center justify-between p-4 bg-white ">
             <div className="flex items-center gap-4">
+                <img src="/utilities/T-Sender.svg" alt="TSender logo" className="w-18 h-18"></img>
+
+                <h1 className="text-3xl font-bold text-gray-800">TSender</h1>
                 <a
                 href="https://github.com/Lokesh0224/Fullstack-web3/tree/main/ts-tsender-ui-cu"
                 target="_blank"
                 rel="noopener noreferrer"
-                className=" bg-red-500 hover:text-gray-600 transition-colors"
+                className="bg-red-500 w-18 h-18 hover:text-gray-600 transition-colors"
                 >
                     <FaGithub size={30}/>
                 </a>
-                <h1 className="text-3xl font-bold text-gray-800">TSender</h1>
             </div>
             <ConnectButton/>
         </header>
