c8d764ebce1691b9f29dfc682c7066b30dd97d3d
Lokesh0224 <lokeshk20019@gmail.com>
2025-08-09 10:36:37 +0530

    button functionality

--- ts-tsender-ui-cu/src/components/AirdropForm.tsx
+++ ts-tsender-ui-cu/src/components/AirdropForm.tsx
@@ -1,18 +1,54 @@
 "use client"
 import { useState } from "react";
 import InputField from "./ui/InputField";
+import {chainsToTSender, erc20Abi, tsenderAbi} from "@/constants"
+import {useChainId, useConfig, useAccount} from 'wagmi'
+import {readContract} from '@wagmi/core'
 
 export default function AirdropForm(){
 
     const [tokenAddress, setTokenAddress] = useState("")
     const [recipients, setRecipients] = useState("")
     const [amount, setAmount] = useState("")
+    const chainId = useChainId()
+    const config = useConfig()
+    const account = useAccount()//address of the one who is gonna airdrop to the recipients
+
+    /*  
+        tokenAddress	The address of the ERC-20 token contract
+        account.address	Your wallet address (the token owner)
+        tSenderAddress	The TSender contract address (the spender) 
+    */
+    async function getApprovedAmount(tSenderAddress: string | null): Promise<number> {
+        if(!tSenderAddress){
+            alert("No address found, please use a supported chain")
+            return 0;
+        }
+        //read from the chain that we've approved enough tokens
+        const response = await readContract(config, {
+            abi: erc20Abi, //erc20 contract abi
+            address: tokenAddress as `0x${string}` , //address of the ERC-20 token contract that you want to airdrop.
+            functionName: "allowance", 
+            /* function allowance(address owner, address spender) external view returns (uint256); 
+
+            owner: the address that owns the tokens (i.e., your wallet)
+            spender: the address that is allowed to spend those tokens (i.e., your TSender contract)*/
+
+            args:[account.address, tSenderAddress as `0x${string}`] // Check how many tokens your wallet (owner) has approved for the TSender contract (spender) to use
+        })
+        return response as number
+
+    }
 
     async function handleSubmit(){
-        console.log(tokenAddress)
-        console.log(recipients)
-        console.log(amount)
+        /* address of the Tsender airdrop smart contract that is 
+        deployed to the chain that is currently connected to. */
+        const tSenderAddress = chainsToTSender[chainId]["tsender"]//my contract address deployed on different chains
+        const approvedAmount = await getApprovedAmount(tSenderAddress)
+        console.log(approvedAmount)
+        
     }
+
     return(
         <div>
             <InputField 
@@ -38,7 +74,8 @@ export default function AirdropForm(){
                 large={true}
             />
 
-            <button onClick={handleSubmit}>
+            <button onClick={handleSubmit}
+                className="mt-4 px-6 py-2 bg-blue-600 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 active:scale-95 transition-all duration-200 ease-in-out focus:outline-none">
                 SendTokens
             </button>
 
