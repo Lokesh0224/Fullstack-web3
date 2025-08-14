3400b69ad27f6abaa683de53fadbbbac4a04df8c
Lokesh0224 <lokeshk20019@gmail.com>
2025-08-14 13:35:47 +0530

    added playwright test

--- ts-tsender-ui-cu/src/app/page.tsx
+++ ts-tsender-ui-cu/src/app/page.tsx
@@ -1,10 +1,21 @@
-import Image from "next/image";
-import AirdropForm from "@/components/AirdropForm";
-
+"use client"
+import HomeContent from "@/components/HomeComponents";
+import { useAccount } from "wagmi";
 export default function Home() {
+  const {isConnected} = useAccount(); //this returns object so we're just getting one value from the returned object
   return (
     <div className="">
-      <AirdropForm/>
+      {isConnected ? (
+          <div>
+            <HomeContent/> 
+          </div> 
+        ) : (
+          <div> 
+            Please connect a wallet...
+          </div>
+        )
+      }
+      
     </div>
   );
 }
